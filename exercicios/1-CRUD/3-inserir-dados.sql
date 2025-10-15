fe173151b5b5dece90fa1fe79271454bbab14c22
Samuel Costa <84139864+Samuel1810@users.noreply.github.com>
2025-10-15 16:39:34 +0000

    Insert new data to table - Exercise 3 Done

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,14 @@
 -- Insira 3 classes salariais na tabela salario_classe
-
+INSERT INTO salario_classe(id, salario, nivel)
+VALUES
+(1, 3500, 'Estagiário'),
+(2, 4700, 'Iniciante - grade 1'),
+(3, 5300, 'Iniciante - grade 2');
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+INSERT INTO salario_classe(salario, nivel)
+VALUES
+(8500, 'Gerente'),
+(10700, 'Coordernação de Área'),
+(6500, 'Engenheiro de Qualidade de Software Mid-level'),
+(8000, 'Desenvolvedor Sênior');
