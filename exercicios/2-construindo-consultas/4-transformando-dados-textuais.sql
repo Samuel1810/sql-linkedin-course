52a69a10af7438849f46f07319ec90fe2d39dc50
Samuel Costa <84139864+Samuel1810@users.noreply.github.com>
2025-10-21 13:06:41 +0000

    Text operations and interactions - Exercise 4

--- exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
+++ exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
@@ -1,16 +1,30 @@
 -- Crie uma consulta que retorne as colunas CustomerId, FirstName, LastName e Address das pessoas consumidoras brasileiras
-
+SELECT CustomerId, FirstName AS 'Nome', LastName AS 'Sobrenome', Address AS 'Endereço'
+FROM customers
+WHERE Country LIKE 'Brazil'
 
 -- Na consulta anterior, converta o sobrenome para letras maiúsculas
-
+SELECT CustomerId, FirstName AS 'Nome', UPPER(LastName) AS 'Sobrenome', Address AS 'Endereço'
+FROM customers
+WHERE Country LIKE 'Brazil'
 
 -- Na consulta anterior, converta o nome para letras minúsculas
+SELECT CustomerId, LOWER(FirstName) AS 'Nome', UPPER(LastName) AS 'Sobrenome', Address AS 'Endereço'
+FROM customers
+WHERE Country LIKE 'Brazil'
 
 
 -- Na consulta anterior, adicione uma coluna cujos os dados seja o resultado da concatenação das colunas de nome e sobrenome
-
+SELECT CustomerId, FirstName AS 'Nome', UPPER(LastName) AS 'Sobrenome', Address AS 'Endereço', FirstName || ' ' || LastName AS 'Nome completo'
+FROM customers
+WHERE Country LIKE 'Brazil'
 
 -- Na consulta anterior, substitua "Av." por "Avenida" na coluna endereço
-
+SELECT CustomerId, FirstName AS 'Nome', UPPER(LastName) AS 'Sobrenome', REPLACE(Address, 'Av.', 'Avenida') AS 'Endereço'
+FROM customers
+WHERE Country LIKE 'Brazil'
 
 -- Na consulta anterior, utilize o símbolo de coringa para construir padrões textuais
+SELECT CustomerId, FirstName AS 'Nome', UPPER(LastName) AS 'Sobrenome', REPLACE(Address, 'Av.', 'Avenida') AS 'Endereço'
+FROM customers
+WHERE Country LIKE 'Braz%'
