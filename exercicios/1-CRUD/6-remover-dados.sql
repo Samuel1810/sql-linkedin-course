e8789f453ee6377f2406cb8cd56813d276d9ae4b
Samuel Costa <84139864+Samuel1810@users.noreply.github.com>
2025-10-21 10:45:15 +0000

    Deleting Data - Exercise 6

--- exercicios/1-CRUD/6-remover-dados.sql
+++ exercicios/1-CRUD/6-remover-dados.sql
@@ -1,4 +1,14 @@
 -- Da tabela invoice, remova o invoice cujo o InvoiceID corresponda ao valor 33
+DELETE FROM invoices
+WHERE InvoiceId = 33;
 
 
--- Da tabela invoice, remova todos os invoices cujo o Total seja menor do que 1
+-- Da tabela invoice, remova todos os invoices cujo o Total seja menor do que 1 + Total maior que 16
+DELETE FROM invoices
+WHERE Total < 1 OR Total > 16;
+
+DELETE FROM genres
+WHERE GenreId = 3;
+
+DELETE FROM tracks
+WHERE GenreId = 3;
