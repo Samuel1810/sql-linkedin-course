cf19236b52c147cbdf1442ee58825f1bfcb8bd87
Samuel Costa <84139864+Samuel1810@users.noreply.github.com>
2025-10-21 10:33:49 +0000

    Updating Data - Exercise 5

--- exercicios/1-CRUD/5-atualizar-dados.sql
+++ exercicios/1-CRUD/5-atualizar-dados.sql
@@ -1,7 +1,25 @@
 -- Atualize a classe salarial para a pessoa cujo EmployeeId é igual a 6. Lembre-se que a classe salarial deve corresponder o id_salario da tabela salario_classe
-
+UPDATE employees
+SET id_salario = 3
+WHERE EmployeeId = 6;
 
 -- Atualize a classe salarial e o LastName da pessoa cujo EmployeeId é igual a 2. Lembre-se que a classe salarial deve corresponder o id_salario da tabela salario_classe
+UPDATE employees
+SET id_salario = 4,
+LastName = 'Leão'
+WHERE EmployeeId = 1;
 
 
 -- Atualize a classe salarial 1 para as pessoas cuja coluna id_salario continua NULL. Lembre-se que a classe salarial deve corresponder o id_salario da tabela salario_classe
+UPDATE employees
+SET id_salario = 1
+WHERE id_salario IS NULL
+
+-- Outras 
+UPDATE customers
+SET Company = 'Panasonic'
+WHERE Company IS NULL;
+
+UPDATE playlists
+SET Name = 'A night at the opera'
+WHERE Name = 'Movies';
